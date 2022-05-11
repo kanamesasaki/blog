c0f51349563e019c70ce345d2dcfa126ceeba894
Sasaki <Kaname.Sasaki@dlr.de>
2022-05-11 09:43:10 +0200

    try to fix google analytics issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `G-CCZTCJYRD8`,
+        trackingId: "G-CCZTCJYRD8",
 		head: true,
         anonymize: true,
       },
